918974fdb4b731a5dc0ae13f13dd2557b8135135
sagarsharma6 <sagarbobby6@gmail.com>
2018-08-17 00:06:33 +0530

    Assignment 2

---
+++ Assignment2.py
@@ -0,0 +1,25 @@
+#Ques 1
+print("Hello World!")
+
+#Ques 2
+print("Acad"+"view")
+
+#Ques 3
+x=input()
+y=input()
+z=input()
+print(x,y,z)
+
+#Ques 4
+print("Let's get started");
+
+#Ques 5
+s="Acadview"
+course="Python"
+fees=5000
+print('%s ''%s ''%d'%(s,course,fees))
+
+#Ques 6
+name="Tony Stark"
+salary=1000000
+print('%s ''%d'%(name,salary))
